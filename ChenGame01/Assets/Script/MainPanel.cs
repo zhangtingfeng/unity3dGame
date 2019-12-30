ca686b2e8fc25e3879f00152ff00746afdd2ac30
oliver <1273844711@qq.com>
2019-12-30 11:38:31 +0800

    untiy3d

---
+++ ChenGame01/Assets/Script/MainPanel.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainPanel : MonoBehaviour {
+
+    // Use this for initialization
+    private Button BtntoRecordUI;
+    public GameObject myUIrecordUI;
+    // Use this for initialization
+    void Start()
+    {
+        BtntoRecordUI = this.transform.Find("BtntoRecordUI").GetComponent<Button>();
+        BtntoRecordUI.onClick.AddListener(showRecordUI);
+    }
+
+    private void showRecordUI()
+    {
+        myUIrecordUI.SetActive(true);
+        this.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
