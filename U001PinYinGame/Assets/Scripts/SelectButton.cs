65a4d318c3940f51244cf4fbbedee0ac19e7c30b
oliver <35473477+zhangtingfeng@users.noreply.github.com>
2020-10-26 22:15:46 +0800

    调整按钮位置

--- U001PinYinGame/Assets/Scripts/SelectButton.cs
+++ U001PinYinGame/Assets/Scripts/SelectButton.cs
@@ -18,16 +18,19 @@ public class SelectButton : Assets.Scripts.PunPinYin.pubButton
     void Start()
     {
 
+        int intLeft1 = 534;
+        int intLeft2 = 752;
+
         myButtonSelctList = new ButtonSelect[10];
-        myButtonSelctList[0] = new ButtonSelect { buttonName = "DrawRed", left = 524, top = 165 };
-        myButtonSelctList[1] = new ButtonSelect { buttonName = "PlateDraw", left = 524, top = 228 };
-        myButtonSelctList[2] = new ButtonSelect { buttonName = "VertiDraw", left = 524, top = 299 };
-        myButtonSelctList[3] = new ButtonSelect { buttonName = "SoundModel", left = 524, top = 366 };
-        myButtonSelctList[4] = new ButtonSelect { buttonName = "OneWord", left = 752, top = 136 };
-        myButtonSelctList[5] = new ButtonSelect { buttonName = "TwoWord", left = 752, top = 199 };
-        myButtonSelctList[6] = new ButtonSelect { buttonName = "ThreeWord", left = 752, top = 275 };
-        myButtonSelctList[7] = new ButtonSelect { buttonName = "Story", left = 752, top = 338 };
-        myButtonSelctList[8] = new ButtonSelect { buttonName = "Scan", left = 752, top = 402 };
+        myButtonSelctList[0] = new ButtonSelect { buttonName = "DrawRed", left = intLeft1, top = 165 };
+        myButtonSelctList[1] = new ButtonSelect { buttonName = "PlateDraw", left = intLeft1, top = 232 };
+        myButtonSelctList[2] = new ButtonSelect { buttonName = "VertiDraw", left = intLeft1, top = 299 };
+        myButtonSelctList[3] = new ButtonSelect { buttonName = "SoundModel", left = intLeft1, top = 366 };
+        myButtonSelctList[4] = new ButtonSelect { buttonName = "OneWord", left = intLeft2, top = 136 };
+        myButtonSelctList[5] = new ButtonSelect { buttonName = "TwoWord", left = intLeft2, top = 202 };
+        myButtonSelctList[6] = new ButtonSelect { buttonName = "ThreeWord", left = intLeft2, top = 276 };
+        myButtonSelctList[7] = new ButtonSelect { buttonName = "Story", left = intLeft2, top = 350 };
+        myButtonSelctList[8] = new ButtonSelect { buttonName = "Scan", left = intLeft2, top = 424 };
         myButtonSelctList[9] = new ButtonSelect { buttonName = "Exit", left = 18, top = 479 };
     }
 
