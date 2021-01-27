e43a9b24f5893622e910ad1a4b060a6d089c98b4
oliver <35473477+zhangtingfeng@users.noreply.github.com>
2021-01-27 17:05:53 +0800

    C:\Works\gitworks202101\Assets\Scripts\Sub\OneWord
    C:\Works\gitworks202101\Assets\Scripts\Sub\OneWord

---
+++ U001PinYinGame/Assets/Scripts/Sub/OneWordButton.cs
@@ -0,0 +1,77 @@
+﻿using Assets.Script.PunPinYin;
+using Assets.Scripts.PunPinYin;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class OneWordButton : Assets.Scripts.PunPinYin.pubButton
+{
+
+
+
+
+
+    void Start()
+    {
+        int intFisrtTop = 100;
+        int intStep = 70;
+
+        myButtonSelctList = new ButtonSelect[4];
+        myButtonSelctList[0] = new ButtonSelect { buttonName = "Exit", left = 18, top = 479 };
+        myButtonSelctList[1] = new ButtonSelect { buttonName = "PlaySound", left = 906, top = intFisrtTop, width = 100, playSound = false };
+        myButtonSelctList[2] = new ButtonSelect { buttonName = "Pre", left = 906, top = intFisrtTop + intStep * 1, width = 100, playSound = false };
+        myButtonSelctList[3] = new ButtonSelect { buttonName = "Next", left = 906, top = intFisrtTop + intStep * 2, width = 100, playSound = false };
+        //myButtonSelctList[4] = new ButtonSelect { buttonName = "Record", left = 906, top = intFisrtTop + intStep * 3, width = 77, playSound = false, boolReapeatButton = true, ReapeatButtonStartAction= "RecordStart", ReapeatButtonStopAction = "RecordStop" };
+        //myButtonSelctList[5] = new ButtonSelect { buttonName = "RecordStop", left = 906, top = intFisrtTop + intStep * 4, width = 77, playSound = false };
+       // myButtonSelctList[4] = new ButtonSelect { buttonName = "RecordPlaySound", left = 906, top = intFisrtTop + intStep * 5, width = 77, playSound = false };
+
+    }
+
+    override
+    public void MainSelect(string strWhich)
+    {
+        //StaticGlobalService.getTargetItemListPath();
+        switch (strWhich)
+        {
+            case "Exit":
+                GameObject myCameraresetMicrophone = GameObject.Find("Main Camera"); myCameraresetMicrophone.SendMessage("resetMicrophone");
+                UnityEngine.SceneManagement.SceneManager.LoadScene("Select");
+                break;
+            case "PlaySound":
+            case "Pre":
+            case "Next":
+            //case "RecordStart":
+            //case "RecordStop":
+            //case "RecordPlaySound":
+                GameObject myCamera = GameObject.Find("Main Camera");
+
+                myCamera.SendMessage("ButtonAction", strWhich);
+                break;
+
+                // GameObject myCamera = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject;
+
+                // UnityEngine.SceneManagement.SceneManager.LoadScene("Select");
+                //break;
+        }
+        // UnityEngine.SceneManagement.SceneManager.LoadScene(strWhich);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+
+
+
+
+
+
+}
---
+++ U001PinYinGame/Assets/Scripts/Sub/OneWordCameraIni.cs
@@ -0,0 +1,210 @@
+﻿using Assets.Script.PunPinYin;
+using Assets.Scripts.Pub;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OneWordCameraIni : MonoBehaviour
+{
+
+
+    public Text myText;
+    public RawImage myRawImage;
+    public RawImage TextPinYin;
+    private List<string> GloaltextList;
+    private int ThisOlderNum = -1;
+    private String PathLetter = "";
+
+    private LoadRawImage LoadRawImageService = new LoadRawImage();
+    void Start()
+    {
+
+
+
+        PathLetter = StaticGlobal.getLetterPath();
+
+        if (Directory.Exists(PathLetter))
+        {
+
+            //string[] ddddd = System.IO.Directory.GetFiles(bb,);
+            IOrderedEnumerable<String> textList = Directory.GetFiles(PathLetter, "*.*", SearchOption.TopDirectoryOnly).Where(s => s.EndsWith(".txt")).OrderBy(s => s.ToString()); ;
+            GloaltextList = textList.ToList();
+            Next();
+        }
+    }
+
+    void Pre()
+    {
+        if (ThisOlderNum > 0)
+        {
+            ThisOlderNum--;
+            loadTextAndPic();
+        }
+    }
+
+
+    void Next()
+    {
+
+        if (ThisOlderNum < GloaltextList.Count - 1)
+        {
+            ThisOlderNum++;
+            loadTextAndPic();
+        }
+    }
+
+
+    void loadTextAndPic()
+    {
+        String strPathmyText = GloaltextList[ThisOlderNum].toString();
+
+
+
+        string sssContent = File.ReadAllText(strPathmyText);
+        myText.text = sssContent;
+
+
+
+        String strPathmyPic = (strPathmyText.Substring(0, strPathmyText.Length - 3) + "jpg");
+        LoadRawImageService.showLocalFile(myRawImage, strPathmyPic);
+
+
+        String strTextPinYinPic = (strPathmyText.Substring(0, strPathmyText.Length - 4) + "P.png");
+        LoadRawImageService.showLocalFile(TextPinYin, strTextPinYinPic);
+
+
+        StartCoroutine(checkRecordSoundButtonStatus(0.2f));
+
+
+
+        GameObject myCamera = GameObject.Find("Main Camera");
+        myCamera.SendMessage("stopPlayLocalFile");
+
+
+        /*
+        //加载图片方式3;
+        //filePath = Application.dataPath + @"/_Image/grid.png";
+        FileStream fs = new FileStream(strPathmyPic, FileMode.Open, FileAccess.Read);
+        System.Drawing.Image img = System.Drawing.Image.FromStream(fs);
+        //System.Drawing.Image.FromFile(filePath); //方法二加载图片方式。
+
+        MemoryStream ms = new MemoryStream();
+        img.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+
+        Texture2D _tex2 = new Texture2D(128, 128);
+        _tex2.LoadImage(ms.ToArray());
+
+
+
+        //WWW _wwwstrPathmyPic = new WWW(strPathmyPic);
+        myRawImage.texture = _tex2;
+        //WWW_Load_RawImage.LoadTexture2DByWWW(strPathmyPic, myRawImage);
+
+        Debug_Log.Call_WriteLog(strPathmyPic, "Directory.ExistsstrPathmyPic");
+        //GetSprite()
+        //myRawImage =*/
+
+    }
+
+    IEnumerator checkRecordSoundButtonStatus(float waitSoundSecod)
+    {
+        yield return new WaitForSeconds(waitSoundSecod);//等待一秒后异步加载
+        GameObject gameObjectRecordPlayButton = GameObject.Find("RecordPlayButton");
+        if (File.Exists(GetRecordname()))
+        {
+            gameObjectRecordPlayButton.SendMessage("SetRecordPlayStatus", true);
+        }
+        else
+        {
+            gameObjectRecordPlayButton.SendMessage("SetRecordPlayStatus", false);
+        }
+    }
+    String GetRecordname()
+    {
+        string strgetLetterRecordPath = StaticGlobal.getLetterRecordPath();
+        String strPathmyText1 = GloaltextList[ThisOlderNum].toString();
+        String strPathmyWav1 = (strPathmyText1.Substring(0, strPathmyText1.Length - 3) + "wav");
+        String FileName = System.IO.Path.GetFileNameWithoutExtension(strPathmyWav1);
+
+        String strRecordName = Path.Combine(strgetLetterRecordPath, FileName + ".wav");
+
+        return strRecordName;
+    }
+
+    void ButtonAction(String strstrWhichH)
+    {
+        GameObject myCamera = GameObject.Find("Main Camera");
+        //String strH111 = strH;
+        //Debug_Log.Call_WriteLog(strH111, "这里是安卓设备ButtonAction");
+        switch (strstrWhichH)
+        {
+
+            case "PlaySound":
+                myCamera.SendMessage("resetMicrophone");
+                String strPathmyText = GloaltextList[ThisOlderNum].toString();
+                String strPathmyWav = (strPathmyText.Substring(0, strPathmyText.Length - 3) + "wav");
+
+
+
+
+
+                // string strPath = StaticGlobal.getLetterPath();
+                // String straudioPath = Path.Combine(strPathmyWav, "01.wav");
+                myCamera.SendMessage("PlayLocalFile", strPathmyWav);
+                break;
+            case "Pre":
+                myCamera.SendMessage("resetMicrophone");
+                Pre(); break;
+            case "Next":
+                myCamera.SendMessage("resetMicrophone");
+                Next(); break;
+            case "RecordStart":
+                myCamera.SendMessage("resetMicrophone");
+                myCamera.SendMessage("StartMicrophone", GetRecordname());
+
+
+
+                break;
+
+            case "RecordStop":
+                myCamera.SendMessage("StopMicrophone");
+                StartCoroutine(checkRecordSoundButtonStatus(1.5f));
+                StartCoroutine(checkRecordSoundButtonStatus(3.5f));
+                break;
+
+            case "RecordPlaySound":
+                //GameObject myCamera = GameObject.Find("Main Camera");
+                if (File.Exists(GetRecordname()))
+                {
+                    myCamera.SendMessage("PlayLocalFile", GetRecordname());
+                }
+                break;
+        }
+    }
+    /*
+    private int intwidth;
+    private int intheight;
+    public Text TextWH;
+    void Start()
+    {
+        intwidth = Screen.width;
+        intheight = Screen.height;
+        print("intwidth="+ intwidth + "  intheight="+ intheight);
+        Debug.Log("intwidth=" + intwidth + "  intheight=" + intheight);
+
+        TextWH.text = "intwidth=" + intwidth + "  intheight=" + intheight;
+
+    }*/
+    // Use this for initialization
+    void Awake()
+    {
+        //DontDestroyOnLoad(transform.gameObject);////跨场景播放不销毁
+    }
+
+
+}
